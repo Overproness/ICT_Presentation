c4f89236df77fb9d136c10b1311bfdae32746ffc
Overproness <mr.overproness@gmail.com>
2023-10-01 20:34:47 +0500

    Backend

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
+// import axios from "axios";
+
 const IncrementButton = () => {
   const element = document.getElementById("incrementText");
   let value = element.innerHTML;
   ++value;
-  console.log(value);
   document.getElementById("incrementText").innerHTML = value;
+};
+
+const baseUrl = "http://localhost:3001/";
+
+const GetNotes = async () => {
+  try {
+    const response = await axios.get(baseUrl);
+    const notes = response.data;
+    Adder(notes);
+    return notes;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const Adder = (notes) => {
+  const element = document.getElementById("add-notes");
+  if (notes) {
+    notes.map((note) => {
+      let li = document.createElement("li");
+      li.innerText = note.content;
+      element.appendChild(li);
+    });
+  }
 };
