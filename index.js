0999400c571ec669b05d267018b33ef2cca06925
Overproness <mr.overproness@gmail.com>
2023-10-08 21:12:04 +0500

    Added DataBase

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
+const Note = require("./models/note");
 
 app.use(cors());
-app.get("", (request, response) => {
-  response.json(notes);
+
+app.get("/", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Hello World. Server running on PORT ${PORT}`);
 });
